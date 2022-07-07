a1ed388e89638ae7329a2666243bbcc5772c679e
Thịnh <danghaithinh2001@gmail.com>
2022-07-08 00:12:36 +0700

    Update front-end
    - add Main.js
    - config Navbar.js
    - add new functions to App.js to load contracts, stake, and unstake tokens

--- defi-staking-app-starter/src/components/Navbar.js
+++ defi-staking-app-starter/src/components/Navbar.js
@@ -5,14 +5,14 @@ import bank from '../bank.png';
 class Navbar extends Component{
     render(){
         return(
-            <nav class="navbar navbar-dark flixed-top shadow p-0" style={{backgroundColor: 'black', height: '50px'}}>
-                <a class="navbar-brand col-sm-3 col-md-2 mr-0"
+            <nav className="navbar navbar-dark flixed-top shadow p-0" style={{backgroundColor: 'black', height: '50px'}}>
+                <a className="navbar-brand col-sm-3 col-md-2 mr-0"
                     style={{color: 'white'}}>
-                        <img src={bank} width="50" height="30" class="d-inline-block align-top" alt="bank" />
+                        <img src={bank} width="50" height="30" className="d-inline-block align-top" alt="bank" />
                         &nbsp; DAPP Yield Staking (Decentralized Bank)
                 </a>
-                <ul class="navbar-nav px-3">
-                    <li class="text-nowrap d-none nav-item d-sm-none d-sm-block">
+                <ul className="navbar-nav px-3">
+                    <li className="text-nowrap d-none nav-item d-sm-none d-sm-block">
                         <small style={{color: 'white'}}>
                             ACCOUNT NUMBER: {this.props.account}
                         </small>
