3aabcd593679f1242f7f7ea67bdd05b691bf19f9
Thịnh <danghaithinh2001@gmail.com>
2022-07-08 16:22:56 +0700

    Update Main.js

--- defi-staking-app-starter/src/components/Main.js
+++ defi-staking-app-starter/src/components/Main.js
@@ -52,7 +52,6 @@ class Main extends Component{
                         onClick={(event) =>{
                             event.preventDefault()
                             this.props.unstakeTokens()
-                            window.location.reload()
                         }}
                         className='btn btn-primary btn-lg btn-block'>WITHDRAW</button>
                         <div className='card-body text-center' style={{color:'blue'}}>
